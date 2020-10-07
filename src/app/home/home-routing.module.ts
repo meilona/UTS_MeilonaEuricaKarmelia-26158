9e9996ec2f0e81125d72cbcd1b28bbc0811814ac
meilona <meilonaeurica@gmail.com>
2020-10-07 17:35:02 +0700

    first commit

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
---
+++ src/app/home/ram.model.ts
@@ -0,0 +1,7 @@
+export interface ram {
+    id: string;
+    title: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+}
